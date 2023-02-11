f90004081b89061d11bfc65765ce48c644487918
Abu Abdullah <abdullah72308@gmail.com>
2023-02-12 04:14:46 +0600

    complete

--- login.js
+++ login.js
@@ -4,10 +4,9 @@ document.getElementById('submit-btn').addEventListener('click', function(){
     const email = emailField.value;
     const passwordField = document.getElementById('user-pass');
     const password = passwordField.value;
-    if(email === 'abdullah@gmail.com' && password === '123456'){
+    if(email === 'bal@bal.com' && password === 'balsal'){
         window.location.href = 'index2.html'
-        console.log('valid user')
     } else{
-        alert('invalid user')
+        alert('password or username in invalid')
     }
 })
